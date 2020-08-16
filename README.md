215e50b402f9eb613d83b9a88360d4026d9253d8
Farid <f.askerov@infoflot.com>
2020-08-16 13:51:38 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hello world.
+
+This is training package. Just echo "Hello world"
---
+++ src/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nifay;
+
+/**
+ * Class HelloWorld
+ * @package Nifay
+ */
+
+class HelloWorld {
+
+    public static function sayHelloWorld()
+    {
+        echo 'Hello World!' . PHP_EOL;
+    }
+
+}
